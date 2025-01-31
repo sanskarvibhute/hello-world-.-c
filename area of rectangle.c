f8b5bb8581c89d76ccb650cf315148415d2c240a
sanskarvibhute <sanskar.vibhute18@gmail.com>
2025-01-31 17:28:29 +0530

    Add files via upload

---
+++ area of rectangle.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+int main()
+{
+    int a, b;
+    // area of rectangle
+    printf("enter first side ");
+    scanf("%d", &a);
+    printf("enter second side ");
+    scanf("%d", &b);
+    printf("area of rectangle is= %d\n", a * b);
+    return 0;
+}
