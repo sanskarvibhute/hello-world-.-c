a0647c6ff487f055a69f816187f5ce851be694e9
sanskarvibhute <sanskar.vibhute18@gmail.com>
2025-01-31 17:31:41 +0530

    Add files via upload

---
+++ area of circle.c
@@ -0,0 +1,12 @@
+// area of circle
+#include <stdio.h>
+int main()
+{
+    float r;
+    // let radious of circle=r
+    printf("enter radiousof circle ");
+    scanf("%f", &r);
+    printf("area of circle is= %f\n", r * r * 22 / 7);
+
+    return 0;
+}
