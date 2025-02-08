aa49a7619d98c9c0814d4e213f6fd5bbfd2c9f1c
sanskarvibhute <sanskar.vibhute18@gmail.com>
2025-02-08 19:31:36 +0530

    Add files via upload

---
+++ 2D arryas formation.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main()
+{
+    int i, j, count, count1;
+    printf("enter the number of elements you want to enter ROW's in an array\n");
+    scanf("%d", &count);
+
+    printf("enter the number of elements you want to enter COLUMN's in an array\n");
+    scanf("%d", &count1);
+
+    int marks[count][count1];
+
+    for (int i = 0; i < count; i++)
+    {
+        for (int j = 0; j < count1; j++)
+        {
+            printf("enter the value of %d element of an array \n", i);
+            scanf("%d", &marks[i][j]);
+        }
+    }
+    for (int i = 0; i < count; i++)
+    {
+        for (int j = 0; j < count1; j++)
+        {
+            printf("the entered the value of %d,%d element of an array is %d \n", i, j, marks[i][j]);
+        }
+    }
+}
