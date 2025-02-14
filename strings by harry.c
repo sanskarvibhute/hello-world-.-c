846e93b7b060c9c8631b928f8c12f40b3a360b83
sanskarvibhute <sanskar.vibhute18@gmail.com>
2025-02-14 21:27:43 +0530

    Add files via upload

---
+++ strings by harry.c
@@ -0,0 +1,25 @@
+// this content is from harry bhai's video no.34
+
+#include <stdio.h>
+
+void printStr(char str[])
+{
+    int i=0;
+    while(str[i]!='\0')
+    {
+        printf("%c", str[i]);
+        i++;
+    }
+    printf("\n");
+}
+int main()
+{
+    char str[77];
+    gets(str);
+    printf("Using custom function printStr\n");
+    printStr(str);
+    printf("Using printf %s\n", str);
+    printf("using puts: \n");
+    puts(str);
+    return 0;
+}
