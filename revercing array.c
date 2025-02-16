5a9f6012ae82b7c1c262e87be674d9f59c63da9f
sanskarvibhute <sanskar.vibhute18@gmail.com>
2025-02-16 14:34:01 +0530

    Add files via upload

---
+++ revercing array.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+void arrayRev(int arr[], int numlen)
+{
+    int temp;
+    
+    for (int i = 0; i < numlen / 2; i++)
+    {
+        // swap item arr[i] with arr[numlen-1-i]
+        temp = arr[i];
+        arr[i] = arr[numlen - 1 - i];
+        arr[numlen - 1 - i] = temp;
+    }
+}
+
+void arrayPrint(int arr[], int numlen)
+{
+    for (int i = 0; i < numlen; i++)
+    {
+        printf("The value of element %d is %d\n", i, arr[i]);
+    }
+}
+
+int main()
+{
+    int numlen;
+    printf("Enter the number of elements in the array\n");
+    scanf("%d", &numlen);
+    int arr[numlen];
+    for (int j = 0; j < numlen; j++)
+    {
+        printf("enter the value of %d element of an array \n", j);
+        scanf("%d", &arr[j]);
+    }
+
+    printf("Before reversing the array\n");
+    arrayPrint(arr, numlen);
+    arrayRev(arr, numlen);
+    printf("\nAfter reversing the array\n");
+    arrayPrint(arr, numlen);
+
+    return 0;
+}
