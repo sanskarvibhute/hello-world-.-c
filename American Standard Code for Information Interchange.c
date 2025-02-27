03a8ac394878dd988985c46bd515905181be748a
sanskarvibhute <sanskar.vibhute18@gmail.com>
2025-02-27 11:20:07 +0530

    ASCII value.c

---
+++ American Standard Code for Information Interchange.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    char n;
+    printf("enter a character then I will show it's ASCII value: ");
+    scanf("%c", &n);
+    printf("the no. of ASCII (American Standard Code for Information Interchange) is %d", n);
+
+    return 0;
+}
