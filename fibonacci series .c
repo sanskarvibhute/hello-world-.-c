c415bf127ca5989d63c430640aec0598daacf5c7
sanskarvibhute <sanskar.vibhute18@gmail.com>
2025-02-28 12:48:30 +0530

    Update fibonacci series .c

--- fibonacci series .c
+++ fibonacci series .c
@@ -17,6 +17,33 @@ int main()
     }
     return 0;
 }
+----------------------------------------------------------------------------
+#include <stdio.h>
+int main() {
+    int n, a = 0, b = 1, sum;
+
+    printf("Enter total number of terms: ");
+    scanf("%d", &n);
+    
+    for (int i = 1; i <= n; i=i+1){
+    if(i==1){
+       printf("%d\n",a);
+    }
+    else if(i==2){
+       printf("%d\n",b);
+    
+    }
+    else if(i>2){
+        sum = a + b;
+        a = b;
+        b = sum;
+        printf("%d\n", sum);
+        }
+    }
+    return 0;
+}
+.........................................................
+    
 
 // Fibonacci series in Recursion:
 
