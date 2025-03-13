e1d740519bd3a647f99b99367bdbfb4f00c5724b
sanskarvibhute <sanskar.vibhute18@gmail.com>
2025-03-13 10:53:32 +0530

    Update accessing marks.c

--- accessing marks.c
+++ accessing marks.c
@@ -23,6 +23,7 @@ int main()
 }
 
 // THIS CAN ALSO DONE WITH MEMORY ALLOCATION METHOD
+//method 1 by using   malloc 
 #include <stdio.h>
 #include <stdlib.h>
 int main()
@@ -48,5 +49,30 @@ int main()
 
     return 0;
 }    
+
+// method 2 by  calloc
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+    int n:
+    printf("enter no.of marks enter in array\n");
+    scanf("%d", &d);
+
+    int * marks = (int *)calloc(n, sizeof(int));
+    for (int i = 0; i < n; i++ )
+    {   
+        printf("mark of student at id %d: ", i+1);
+        scanf("%d", &marks[i]);
+    }
+    for (int i = 0; i < n; i++)
+    {
+        printf("your entered value for student id %d: ", i+1);
+        printf("is at memory location = %p:", (void*)&marks[i]);
+        printf("\t\tis = %d:", marks[i]);
+        printf("\n")
+        }
+    return 0;
+}
+    
     
 
