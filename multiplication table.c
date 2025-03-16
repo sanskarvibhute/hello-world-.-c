91a8a53dafcbc10391de685b44efb4c97e056302
sanskarvibhute <sanskar.vibhute18@gmail.com>
2025-03-16 20:57:15 +0530

    Update multiplication table.c

--- multiplication table.c
+++ multiplication table.c
@@ -41,3 +41,19 @@ int main()
 
     return 0;
 }
+
+// multiplication table using for loop
+#include <stdio.h>
+int main()
+{
+    int n;
+    printf("Enter the number of rows you want in the multiplication table: \n");
+    scanf("%d", &n);
+    int m;
+    printf("Enter the number of columns you want in the multiplication table: \n");
+    scanf("%d", &m);
+    for (int i = 1; i <= m; i++)
+    {
+        printf("%d x %d = %d\n", n, i, n * i);
+    }
+}
